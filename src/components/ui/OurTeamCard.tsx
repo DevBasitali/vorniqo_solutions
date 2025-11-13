36fc997cea8432f295a550ff2c379c2ac21b8577
@Fawad228 <mfawadkhan05@gmail.com>
2025-11-13 21:56:00 +0500

    createteamcard

--- src/components/ui/OurTeamCard.tsx
+++ src/components/ui/OurTeamCard.tsx
@@ -4,9 +4,9 @@ import single_flash from "../../../public/images/services/single_flash.png";
 function OurTeamCard() {
   return (
     <>
-      <div className="relative bg-red-500 h-72 w-72">
+      <div className="relative bg-r h-72 w-72">
         <div
-          className="w-64 h-64 p-10 bg-gradient-to-br from-cyan-400 to-blue-600 shadow-lg"
+          className="w-64 h-64 m-auto p-10 bg-gradient-to-br from-cyan-400 to-blue-600 shadow-lg"
           style={{
             clipPath:
               "polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%)",
@@ -17,10 +17,15 @@ function OurTeamCard() {
             neque?
           </p>
         </div>
-        <div className="absolute top-10 -right-10">
-          <Image src={single_flash} alt="single-flash" />
-
-          {/* </Image> */}
+        {/* Flash icon */}
+        <div className="absolute top-2 right-16">
+          <Image src={single_flash} alt="single-flash" width={50} height={50} />
+        </div>
+        {/* descreption */}
+        <div className="flex flex-col justify-center items-center">
+          <h6>M.TALHA</h6>
+          <p>CEO FOUNDER OF</p>
+          <p>VORNIQO SOLUTIONS</p>
         </div>
       </div>
     </>
