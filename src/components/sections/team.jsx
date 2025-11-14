0832c464ece1e4f10f9ffe8be9514b5af681d7d9
@Fawad228 <mfawadkhan05@gmail.com>
2025-11-14 20:22:27 +0500

    team card

--- src/components/sections/team.jsx
+++ src/components/sections/team.jsx
@@ -1,22 +1,62 @@
 import React from "react";
 import OurTeamCard from "@/components/ui/OurTeamCard";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 function Team() {
   return (
     <>
-      <div className="h-full">
-        <div className="border-t-[1px] w-full border-t-white ">
+      <div className="">
+        <div className="border-t-[1px] w-full border-t-white/30 ">
           {/* heading */}
-          <div className="border-l-[1px] flex justify-center items-center border-r-[1px] border-r-white h-24 border-l-white w-1/2 m-auto ">
+          <div className="border-l-[1px] flex justify-center items-center border-r-[1px] border-r-white/30 h-24 border-l-white/30 w-1/2 m-auto">
             <h1 className="text-center">MEET OUR TEAM</h1>
           </div>
-          <div className="border-t-[1px] border-t-white">
-            <div className="w-[90%] m-auto p-16  border-r-[1px] border-r-white h-[100vh] border-l-[1px] border-l-white">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi,
-                numquam.
-              </p>
-              <div className="my-10">
+          <div className="border-t-[1px] border-t-white/30">
+            <div className="w-[90%] border-r-[1px] m-auto border-r-white/30 border-l-[1px] border-l-white/30">
+              <div className="p-10 gap-10 flex">
+                {/* <OurTeamCard></OurTeamCard>
                 <OurTeamCard></OurTeamCard>
+                <OurTeamCard></OurTeamCard>
+                <OurTeamCard></OurTeamCard>
+                <OurTeamCard></OurTeamCard>
+                <OurTeamCard></OurTeamCard> */}
+                <Carousel className="w-full flex justify-center">
+                  <CarouselContent className="flex">
+                    {Array.from({ length: 8 }).map((_, i) => (
+                      <CarouselItem
+                        key={i}
+                        className="
+          basis-1/2        /* xs & sm → 2 per view */
+          lg:basis-1/4     /* lg → 4 per view */
+          2xl:basis-1/5    /* 2xl → 5 per view */
+          flex justify-center
+        "
+                      >
+                        <OurTeamCard />
+                      </CarouselItem>
+                    ))}
+                  </CarouselContent>
+
+                  <CarouselPrevious />
+                  <CarouselNext />
+                </Carousel>
+
+                {/* <button
+                  className="w-12 h-12 bg-red-500"
+                  style={{
+                    clipPath:
+                      "polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%)",
+                  }}
+                >
+                  <ChevronLeftIcon size={25} />
+                  <ChevronRightIcon size={25} />
+                </button> */}
               </div>
             </div>
           </div>
--- src/components/ui/OurTeamCard.tsx
+++ src/components/ui/OurTeamCard.tsx
@@ -1,31 +1,22 @@
 import React from "react";
 import Image from "next/image";
 import single_flash from "../../../public/images/services/single_flash.png";
+import talha from "../../../public/images/team/talha.png";
+// import daniyal from "../../../public/images/team/daniyal.png";
+// import hera from "../../../public/images/team/hera.png";
+// import shafay from "../../../public/images/team/shafay.png";
+// import ahmad from "../../../public/images/team/ahmad.png";
+// import fawad from "../../../public/images/team/fawad.png";
 function OurTeamCard() {
   return (
     <>
-      <div className="relative bg-r h-72 w-72">
-        <div
-          className="w-64 h-64 m-auto p-10 bg-gradient-to-br from-cyan-400 to-blue-600 shadow-lg"
-          style={{
-            clipPath:
-              "polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%)",
-          }}
-        >
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
-            neque?
-          </p>
-        </div>
-        {/* Flash icon */}
-        <div className="absolute top-2 right-16">
-          <Image src={single_flash} alt="single-flash" width={50} height={50} />
-        </div>
+      <div className="flex flex-col justify-center w-fit">
+        <Image src={talha} className="" alt="ceo" />
         {/* descreption */}
-        <div className="flex flex-col justify-center items-center">
-          <h6>M.TALHA</h6>
-          <p>CEO FOUNDER OF</p>
-          <p>VORNIQO SOLUTIONS</p>
+        <div className="flex justify-center flex-col items-center">
+          <h3>M.TALHA</h3>
+          <p className="!text-sm">CEO FOUNDER OF</p>
+          <p className="!text-sm">VORNIQO SOLUTIONS</p>
         </div>
       </div>
     </>
